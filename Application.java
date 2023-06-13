5ab791cc56f04913d2e4c3b118eca1a242d5d381
姚文强 <yaowenqiang111@163.com>
2023-06-13 23:38:09 +0800

    add printSchema demo

--- Application.java
+++ Application.java
@@ -1,6 +1,11 @@
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SaveMode;
 import org.apache.spark.sql.SparkSession;
+
+import java.util.Properties;
+
+import static org.apache.spark.sql.functions.*;
 
 public class Application {
     public static void main(String[] args) {
@@ -14,6 +19,24 @@ public class Application {
         Dataset<Row> df = spark.read().format("CSV")
                 .option("header", true)
                 .load("name_and_comments.txt");
+
+        //transformation
+
+//        df = df.withColumn("full_name", concat(df.col("last_name"), lit(", "), df.col("first_name")));
+        df = df.withColumn("full_name",
+            concat(df.col("last_name"), lit(", "), df.col("first_name")))
+            .filter(df.col("comment").rlike("\\d+"))
+            .orderBy(df.col("last_name").asc());
+
+        String dbConnectionUrl = "jdbc:postgresql://localhost/course_data";
+        Properties prop = new Properties();
+        prop.setProperty("driver", "org.postgresql.Driver");
+        prop.setProperty("user","postgres");
+        prop.setProperty("password", "password");
+
+        df.write()
+            .mode(SaveMode.Overwrite)
+            .jdbc(dbConnectionUrl, "project1", prop);
 
         df.show();
 
---
+++ PrintSchemaApplication.java
@@ -0,0 +1,7 @@
+
+public class PrintSchemaApplication {
+    public static void main(String[] args) {
+        InferCSVSchema parser = new InferCSVSchema();
+        parser.printSchema();
+    }
+}
