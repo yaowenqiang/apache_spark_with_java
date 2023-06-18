77b84bc13e321973f30f5c492ae5923cfd3440fe
姚文强 <yaowenqiang111@163.com>
2023-06-18 23:40:12 +0800

    add dataset map demo

--- MapAndReduce.java
+++ MapAndReduce.java
@@ -1,41 +1,38 @@
 import org.apache.log4j.Level;
+import org.apache.spark.api.java.function.MapFunction;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Encoders;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 import org.apache.log4j.Logger;
 
+import java.io.Serializable;
 import java.util.Arrays;
 import java.util.List;
-
-public class ArrayToDataset {
+public class MapAndReduce {
     public static void main(String[] args) {
+
         Logger.getLogger("org.apache")
                 .setLevel(Level.WARN);
         SparkSession spark = SparkSession.builder()
-                .appName("Array to Dataset<String>")
+                .appName("Map and Reduce")
                 .master("local")
                 .getOrCreate();
 
-        String[] stringList = new String[] {"Banana", "Car", "Banana", "Computer", "Car"};
+        String[] stringList = new String[] {"banana", "Car", "Glass", "Banana", "Computer", "Car"};
 
         List<String> data = Arrays.asList(stringList);
 
-        // dataframe
-//        Dataset<Row> df = spark.createDataset(data, Encoders.STRING());
-        // dataset
         Dataset<String> ds = spark.createDataset(data, Encoders.STRING());
-
-
-
-        ds.printSchema();
+        ds = ds.map(new StringMapper(), Encoders.STRING());
         ds.show();
-
-
-        Dataset<Row> df = ds.groupBy("value").count();
-        df.show();
-
-        Dataset<Row> df2 = ds.toDF();
-        df2.as(Encoders.STRING()); // return a dataset
     }
+
+    static class  StringMapper implements MapFunction<String, String>, Serializable {
+        @Override
+        public String call(String s) throws Exception {
+            return "word: " + s;
+        }
+    }
+
 }
