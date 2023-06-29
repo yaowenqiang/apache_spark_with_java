d6338de5cda789fec8e7f63f45f2d89d5f349a94
姚文强 <yaowenqiang111@163.com>
2023-06-30 00:15:57 +0800

    finish reddit word count

--- Reddit.java
+++ Reddit.java
@@ -1,8 +1,10 @@
+import com.jobreadyprogrammer.spark.WordUtils;
 import com.twitter.chill.java.ArraysAsListSerializer;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.spark.api.java.function.FlatMapFunction;
 import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Encoders;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 
@@ -31,16 +33,27 @@ public class Reddit {
         redditDf = redditDf.select("body");
         redditDf.show(10);
         Dataset<String> wordsDs = redditDf.flatMap((FlatMapFunction<Row, String>)
-                r -> Arrays.asList(Arrays.stream(r.toString()
+                r -> Arrays.asList(r.toString()
                         .replace("\n", "")
                         .replace("\r", "")
                         .trim()
                         .toLowerCase()
                         .split(" "))
                         .iterator()
-                ),Encoders.STRING);
-
+                , Encoders.STRING()
                 );
+
+        Dataset<Row> wordsDf = wordsDs.toDF();
+        Dataset<Row> boringWordsDf = spark.createDataset(Arrays.asList(WordUtils.stopWords), Encoders.STRING()).toDF();
+
+//        wordsDf = wordsDf.except(boringWordsDf); -- this remove duplicates
+
+        wordsDf = wordsDf.join(boringWordsDf, wordsDf.col("value").equalTo(boringWordsDf.col("value")), "left_anti");
+
+        wordsDf = wordsDf.groupBy("value").count();
+        wordsDf.orderBy(desc("count")).show();
+
+
 
     }
 }
