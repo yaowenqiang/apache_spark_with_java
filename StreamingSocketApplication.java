9b9562810bbaecd47d1357f096312c8a2a78a4c2
姚文强 <yaowenqiang111@163.com>
2023-06-30 23:56:27 +0800

    add spark streaming demo

---
+++ StreamingSocketApplication.java
@@ -0,0 +1,48 @@
+import com.jobreadyprogrammer.spark.WordUtils;
+import com.twitter.chill.java.ArraysAsListSerializer;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Encoders;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.streaming.StreamingQuery;
+
+
+import javax.xml.crypto.Data;
+import java.util.Arrays;
+
+public class StreamingSocketApplication {
+    public static void main(String[] args) {
+
+        // nc -lk 9999
+
+        Logger.getLogger("org.apache")
+                .setLevel(Level.WARN);
+        SparkSession spark = SparkSession.builder()
+                .appName("StreamingSocketApplication")
+                .master("local")
+                .getOrCreate();
+
+        Dataset<Row> lines = spark
+                .readStream()
+                .format("socket")
+                .option("host", "localhost")
+                .option("port", 9999)
+                .load();
+
+        Dataset<String> words = lines
+                .as(Encoders.STRING())
+                .flatMap((FlatMapFunction<String, String>) x -> Arrays.asList(x.split(" ")).iterator(), Encoders.STRING());
+        Dataset<Row> wordCounts = words.groupBy("value").count();
+        StreamingQuery query = wordCounts.writeStream()
+                .outputMode("complete")
+                .format("console")
+                .start();
+        query.awaitTermination();
+
+
+
+    }
+}
